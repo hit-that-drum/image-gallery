65b9bde856b7a2cfdd8fffbfdd15a48ac5d97347
hyeyoungkim <velvetredvelvet91@gmail.com>
2022-08-19 00:13:45 +0900

    기본 스타일링

--- src/Component/Album.js
+++ src/Component/Album.js
@@ -2,19 +2,46 @@ import React from 'react';
 import styled from 'styled-components';
 import folderImg from '../Component/folder.png';
 
-const FolderImg = styled.img`
+const FolderBox = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 100px;
-  height: 100px;
+  height: 150px;
+  padding: 2vh;
+  /* margin: auto; */
+
+  :hover {
+    img:nth-of-type(1) {
+      width: 80px;
+      height: 80px;
+    }
+    div:nth-of-type(1) {
+      font-size: 1rem;
+      color: black;
+    }
+  }
+`
+const FolderImg = styled.img`
+  width: 75px;
+  height: 75px;
+`
+const FolderTitle = styled.div`
+  width: 75px;
+  overflow: visible;
+  text-align: center;
+  font-size: 0.8rem;
+  font-weight: 700;
+  color: grey;
 `
 
 const Album = ({ photos }) => {
   return (
-    <>
-      <div>
+    <FolderBox>
       <FolderImg src={folderImg} />
-      </div>
-      {photos.title}
-    </>
+      <FolderTitle>
+        {photos.title}
+      </FolderTitle>
+    </FolderBox>
   );
 };
 
